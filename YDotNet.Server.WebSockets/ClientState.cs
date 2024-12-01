4167f9aa032f9f0aa6a99ba24aa640cae0cb64f1
Andrey Leskov <andrey.leskov@gmail.com>
2024-12-01 12:39:45 -0800

    make client state manage subdocs access

--- YDotNet.Server.WebSockets/ClientState.cs
+++ YDotNet.Server.WebSockets/ClientState.cs
@@ -1,9 +1,8 @@
 using System.Collections.Concurrent;
 using System.Net.WebSockets;
+using System.Security;
 
 namespace YDotNet.Server.WebSockets;
-
-public record PendingUpdate(string DocId, byte[] Update);
 
 public record SubDocumentContext(string DocId, Queue<byte[]> PendingUpdates, bool IsSynced);
 
@@ -20,8 +19,9 @@ public sealed class ClientState : IDisposable
     required public DocumentContext DocumentContext { get; set; }
 
     public string DocumentName => this.DocumentContext.DocumentName;
-    
-    public readonly ConcurrentDictionary<string,SubDocumentContext>  SubDocuments= new ();
+
+    public IReadOnlyDictionary<string, SubDocumentContext> SubDocuments => _subDocuments;
+    private readonly ConcurrentDictionary<string,SubDocumentContext>  _subDocuments = new(StringComparer.Ordinal);
     public bool IsSynced { get; set; }
     public Queue<byte[]> PendingUpdates { get; } = new();
     
@@ -46,16 +46,34 @@ public sealed class ClientState : IDisposable
             return;
         }
         
-         var subDocument = GetSubDocument(docId);
+         var subDocument = AddSubDocument(docId);
          subDocument.PendingUpdates.Enqueue(update);
     }
-    public SubDocumentContext GetSubDocument(string docId)
+    public SubDocumentContext AddSubDocument(string docId)
     {
-        return SubDocuments.AddOrUpdate(docId,
+        return _subDocuments.AddOrUpdate(docId,
             id=>new SubDocumentContext(id,new Queue<byte[]>(),false),
             (id,ctx) => ctx);
     }
+    
+    public bool TryGetSubDocument(string docId, out SubDocumentContext? subDoc)
+    {
+        return _subDocuments.TryGetValue(docId, out subDoc);
+    }
 
+    public bool TryValidateDocument(string docId)
+    {
+        if (DocumentName == docId) return true;
+        if (SubDocuments.ContainsKey(docId)) return true;
+        return false;
+    }
+    
+    public void ValidateDocument(string docId)
+    {
+       if (!TryValidateDocument(docId))
+           throw new InvalidOperationException($"Invalid document id: {docId}, it is neither state root doc neither a subdocument of {DocumentName}");
+    }
+    
     public void Dispose()
     {
         WebSocket.Dispose();
